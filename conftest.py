0c58d2e2a100999e198e4f5f93b86d19dde7b135
rattus-aristarchus <lankin.m.a@gmail.com>
2023-08-08 20:51:47 +0300

    hack no longer necessary

--- conftest.py
+++ conftest.py
@@ -4,21 +4,7 @@ from selene.support.shared import browser
 from selenium import webdriver
 
 
-@pytest.fixture(scope="module")
-def chrome_options():
-    options = webdriver.ChromeOptions()
-
-    """
-    Без следующей строки браузер запускается пустым. Почему - так и не понял(( Решение отсюда:
-    https://stackoverflow.com/questions/50642308/webdriverexception-unknown-error-devtoolsactiveport-file-doesnt-exist-while-t/66194969#66194969
-    """
-    options.add_argument('--remote-debugging-port=9222')
-
-    return options
-
-
 @pytest.fixture()
-def chrome_driver(chrome_options):
-    driver = webdriver.Chrome(options=chrome_options)
-#    driver.set_window_size(800, 600)
+def chrome_driver():
+    driver = webdriver.Chrome()
     return driver
